e755c754d9d2f17101ecddc3c0e96a353e0e61bb
Bryon Anderson <brkander@ucsc.edu>
2024-04-15 13:06:22 -0700

    push it, push it now!

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,79 +1,74 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - generate landscape with a perlin noise fuction
+// Author: Bryon Anderson
+// Date: 4/15/24
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+/* exported setup, draw */
+let seed = 20;
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
+var t = [];
+var m = 200;
+var xoff = 50;
+var yoff = 0;
+var i = 0.03;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+const sky_color = "#7782B0";
+const cloud_color = "#FFFFFF"
+const ground_color = "#4B543F"
+const rock_color = "#7B6315"
 
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
-
-// setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
+  createCanvas(500, 300, WEBGL);
+  createButton("reload").mousePressed(() => seed++);
+  angleMode(DEGREES);
+  
+  for(var y = 0; y < 60; y++){
+    t.push([]);
+    xoff = 0;
+    for(var x = 0; x < 60; x++){
+      t[y][x] = map(noise(xoff, yoff), 0, 1, -m, m);
+      xoff += i;
+    }
+    yoff += i
+  }
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  randomSeed(seed);
+  background(ground_color);
+  
+  stroke(rock_color);
+  
+  fill(sky_color);
+  rect(-400, -200, width * 6, height/ 2.3);
+  
+  fill("#7B6315")
+  ellipse(-mouseX, -200, 33, 33);   // Top circle
+  
+  fill(cloud_color)
+  const clouds = 30*random();
+  for(let i = 0; i < clouds; i++){
+    let z = random();
+    let x = width * ((random(-1,.20) + ((mouseX/width)/50 + millis() / 500000.0) / z) % 1);
+    let s = -width / 50 / z;
+    let y = random(-100, -65)
+    rect(x, y , x - s / 90, random(10, y/2));
+  }
+  
+  fill(ground_color)
+  rotateX(70);
+  translate(-width/2, -height/2);
+  for(var y = 0; y < 60; y++){
+    beginShape(TRIANGLE_STRIP);
+    for(var x = 0; x < 60; x++){
+      vertex(x * 10, y * 10, t[y][x]);
+      vertex(x * 10, (y + 1) * 10, t[y][x]);
+    }
+    endShape(CLOSE);
+  }
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
 }
