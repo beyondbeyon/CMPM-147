1097df2867d0196d5e9f343ffd4d09399f18b388
Bryon Anderson <brkander@ucsc.edu>
2024-04-23 21:53:52 -0700

    push it boi

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,79 +1,95 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+/* exported generateGrid, drawGrid */
+/* global placeTile */
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+function generateGrid(numCols, numRows) {
+  let grid = [];
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+    for (let j = 0; j < numCols; j++) {
+      let r = noise(i/40,j/40);
+      if(abs(r - 0.5)< 0.03){
+        row.push("w")
+      }
+      else{
+        let a = noise(i/20,j/20);
+        if(a >0.5){
+          row.push(":")
+        }
+        else{
+          row.push("_")
+        }
+      }
     }
-
-    myMethod() {
-        // code to run when method is called
-    }
+    grid.push(row);
+  }
+  
+  return grid;
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
-
-// setup() function is called once when the program starts
-function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
-}
-
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
+function drawGrid(grid) {
+  background(128);
+  const wind = millis()/8000
+  
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  for(let i = 0; i < grid.length; i++) {
+    for(let j = 0; j < grid[i].length; j++) {
+      placeTile(i, j, (4 * pow(noise(mouseY / 100, i, j / 4 + wind), 2)) | 0, 14);
+      
+      if (gridCheck(grid,i,j,"_")) {
+        placeTile(i, j, (4 * pow(random(),10)) | 0, 4);
+      }
+      else{
+        drawContext(grid,i,j,"_",9,3);
+      }
+      
+      if (gridCheck(grid,i,j,":")) {
+        placeTile(i, j, 4 * pow(noise(wind / 10, i, j / 4 + wind),2) | 0, 6);
+      }
+      else{
+        drawContext(grid,i,j,":",4,6,);
+      }
+    }
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function gridCheck(grid, i, j, target) {
+  if(i >= 0 && i < grid.length && j>= 0 && j<grid[i].length){
+    return grid[i][j]==target;
+  }
+  else{
+    return false;
+  }
 }
+
+function gridCode(grid, i, j, target) {
+  return((gridCheck(grid,i--,j,target)<<0) +(gridCheck(grid,i++,j,target)<<3) +
+         (gridCheck(grid,i,j--,target)<<1) + (gridCheck(grid,i,j++,target)<<2))
+}
+
+function drawContext(grid, i, j, target, dti, dtj,inverted = true) {
+  let v = gridCode(grid, i, j, target)
+  if(inverted){
+    v = ~v & 0xf;
+  }
+  let [ti,tj] = lookup[v];
+  placeTile(i, j, dti + ti, dtj + tj);
+}
+
+const lookup = [
+  [1, 1],
+  [1, 0],
+  [0, 1], 
+  [0, 0],
+  [2, 1], 
+  [2, 0], 
+  [1, 1],
+  [1, 0],  
+  [1, 2],
+  [1, 1],
+  [0, 2], 
+  [0, 1], 
+  [2, 2], 
+  [2, 1], 
+  [1, 2], 
+  [1, 1]
+];
